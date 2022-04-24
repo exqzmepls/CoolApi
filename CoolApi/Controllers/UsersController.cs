72080bd4991c620371c893c615511c73faa4505c
exqzmepls <exqzmepls@yandex.ru>
2022-04-24 15:08:09 +0500

    Add users resource annotations

--- CoolApi/Controllers/UsersController.cs
+++ CoolApi/Controllers/UsersController.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using CoolApi.Database;
-using CoolApi.Database.Models;
 using CoolApiModels.Users;
 using System.ComponentModel.DataAnnotations;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace CoolApi.Controllers
 {
@@ -23,12 +20,14 @@ namespace CoolApi.Controllers
             _context = context;
         }
 
-        // GET: api/Users
         [HttpGet]
+        [SwaggerOperation(Summary = "Read users portion.")]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Returns data portion.")]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Invalid query parameters values.")]
         public ActionResult<GetUsersModel> GetUsers(
-            [FromQuery][Required][Range(0, int.MaxValue)] int offset,
-            [FromQuery][Required][Range(1, 50)] int portion,
-            [FromQuery][StringLength(32)] string searchString)
+            [FromQuery, Required, Range(0, int.MaxValue)] int offset,
+            [FromQuery, Required, Range(1, 50)] int portion,
+            [FromQuery, StringLength(32)] string searchString)
         {
             return new GetUsersModel
             {
@@ -41,89 +40,45 @@ namespace CoolApi.Controllers
                     new GetUserModel{Id = Guid.Empty, Login = "user2"}
                 }
             };
-
-            //return await _context.Users.ToListAsync();
         }
 
-        // GET: api/Users/5
         [HttpGet("{id}")]
-        public ActionResult<GetUserModel> GetUser([Required] Guid id)
+        [SwaggerOperation(Summary = "Read user profile info by ID.")]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Returns user profile info.")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, Description = "ID does not exist.")]
+        public ActionResult<GetUserModel> GetUser([SwaggerParameter(Description = "User ID.")] Guid id)
         {
-            /*var user = await _context.Users.FindAsync(id);
-
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return user;*/
-
             return new GetUserModel { Id = id, Login = "user" };
         }
 
-        // PUT: api/Users/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public ActionResult<GetUserModel> PutUser(Guid id, [FromBody][Required] PutUserModel user)
+        [SwaggerOperation(Summary = "Update user details.")]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Returns user updated profile info.")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, Description = "ID does not exist.")]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Invalid operation.")]
+        public ActionResult<GetUserModel> PutUser(
+            [SwaggerParameter(Description = "User ID.")] Guid id,
+            [SwaggerRequestBody(Description = "User new details."), FromBody, Required] PutUserModel user)
         {
-            /*if (id != user.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(user).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UserExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }*/
-
             return new GetUserModel { Id = Guid.Empty, Login = "newLogin" };
         }
 
-        // POST: api/Users
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public ActionResult<GetUserModel> PostUser(PostUserModel user)
+        [SwaggerOperation(Summary = "Create new user.")]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Returns created user profile description.")]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Invalid operation.")]
+        public ActionResult<GetUserModel> PostUser([SwaggerRequestBody(Description = "New user details."), FromBody, Required] PostUserModel user)
         {
-            /*_context.Users.Add(user);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetUser", new { id = user.Id }, user);*/
-
             return new GetUserModel { Id = Guid.Empty, Login = user.Login };
         }
 
-        // DELETE: api/Users/5
         [HttpDelete("{id}")]
-        public IActionResult DeleteUser(Guid id)
+        [SwaggerOperation(Summary = "Delete user.", Description = "Delete user profile, chats and all messages. All messages of other users from chats with this user are also deleted.")]
+        [SwaggerResponse(StatusCodes.Status204NoContent, Description = "User was deleted.")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, Description = "ID does not exist.")]
+        public IActionResult DeleteUser([SwaggerParameter(Description = "User ID.")] Guid id)
         {
-            /*var user = await _context.Users.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            _context.Users.Remove(user);
-            await _context.SaveChangesAsync();*/
-
             return NoContent();
-        }
-
-        private bool UserExists(Guid id)
-        {
-            return _context.Users.Any(e => e.Id == id);
         }
     }
 }
