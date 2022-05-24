ae5a4b14dd70a9bfce0cddc9154b2b844f2e95c7
exqzmepls <exqzmepls@yandex.ru>
2022-05-24 20:24:14 +0500

    Add password hasher

---
+++ CoolApi/Database/Hashers/IPasswordHasher.cs
@@ -0,0 +1,9 @@
+﻿namespace CoolApi.Database.Hashers
+{
+    public interface IPasswordHasher
+    {
+        public string GetPasswordHash(string password);
+
+        public bool VerifyPassword(string passwordHash, string password);
+    }
+}
---
+++ CoolApi/Database/Hashers/MD5PasswordHasher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CoolApi.Database.Hashers
+{
+    public class MD5PasswordHasher : IPasswordHasher
+    {
+        public string GetPasswordHash(string password)
+        {
+            using var hasher = SHA256.Create();
+            var passwordBytes = Encoding.UTF8.GetBytes(password);
+            var hashBytes = hasher.ComputeHash(passwordBytes);
+            var hashString = Convert.ToHexString(hashBytes);
+
+            return hashString;
+        }
+
+        public bool VerifyPassword(string passwordHash, string password)
+        {
+            var providedPasswordHash = GetPasswordHash(password);
+            var result = passwordHash == providedPasswordHash;
+
+            return result;
+        }
+    }
+}
